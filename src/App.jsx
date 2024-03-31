0da4b96accf315e6dfbe5a64c6162fec868e603c
Aef_SB <46419278+aeff60@users.noreply.github.com>
2024-03-31 22:48:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
 
       <div className="meme-container">
-        <img src="./img.png" alt="meme" />
+        <img src="https://cdn.discordapp.com/attachments/976776712908066819/1223278821780750376/image-removebg-preview_2.png?ex=6619462e&is=6606d12e&hm=c1c77216a922922c896b39ef6b766bd6a58cdb290782b1390f42957c064ef193&" alt="meme" />
         <p className="meme-text">{memeText}</p>
       </div>
       <input
